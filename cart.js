d0a813ca50322f313fc31e9c4cabdb2b481516fc
Phoenix Lai <plai40@my.bcit.ca>
2022-03-29 00:46:50 -0700

    fix footer

---
+++ cart.js
@@ -0,0 +1,5 @@
+let cartBack = document.createElement("div")
+cartBack.setAttribute("style", "color: red;, width:300px;, height:300px;, position: absolute")
+cartBack.setAttribute("style", "width:300px;")
+let body = document.getElementsByTagName("body")[0]
+body.append(cartBack)
